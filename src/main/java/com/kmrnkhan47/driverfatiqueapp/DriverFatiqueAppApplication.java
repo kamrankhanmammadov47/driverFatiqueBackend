0472033a98cc63ed0465d93bef457dfea6cf88cf
tonymontana47 <kamrankhan47@mail.ru>
2025-01-10 03:57:16 +0100

    Initial commit - Driver Fatigue Detection Backend

---
+++ src/main/java/com/kmrnkhan47/driverfatiqueapp/DriverFatiqueAppApplication.java
@@ -0,0 +1,13 @@
+package com.kmrnkhan47.driverfatiqueapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverFatiqueAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverFatiqueAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmrnkhan47/driverfatiqueapp/DriverFatiqueAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmrnkhan47.driverfatiqueapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverFatiqueAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
